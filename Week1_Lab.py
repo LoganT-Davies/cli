66e3f507ac8b96a26e1cf97e4f33ec67b854c98d
LoganT-Davies <lognthompson8@hotmail.com>
2025-10-10 16:14:38 +0100

    Week 1 basic python completion

---
+++ Week1_Lab.py
@@ -0,0 +1,9 @@
+print ("Hello World!")
+Student_name = ("Alex")
+Student_age = 20
+print ("Student name:", Student_name)
+print ("Student age:", Student_age)
+num1 = 10
+num2 = 20
+total = (num1+num2)
+print ("The Sum is:", total)
